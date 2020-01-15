bdf38aaeb37c106b053f89fe302204789ad05e62
AldwinBenitez16 <50254341+AldwinBenitez16@users.noreply.github.com>
2020-01-14 21:12:34 -0500

    connected netlify

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I dont know please work
         </a>
       </header>
     </div>
